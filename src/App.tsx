c3f335f5dc7d2b54bb9edb96f54a1b8660175495
Ch1r0ku <romanlysenkowork@gmail.com>
2022-11-09 18:23:48 -0500

    added bootstrap and fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+//import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//common components
+//import Header from './Views/components/header';
+//import Footer from './Views/components/footer';
+
+//content components
+
+
+//Styles and fonts\
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Assignment 2</h1>
     </div>
   );
 }
